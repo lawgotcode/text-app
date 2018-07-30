4c9382aa55fae2a997b49a6f7a19173b26f9efc7
Lawrence Agyei <lawgotcode1990@gmail.com>
2018-07-30 11:31:29 -0500

    output2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Output from './Components/Output';
 import axios from 'axios';
 
 class App extends Component {
@@ -31,7 +32,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       Hello
+       <Output value={this.state.text} />
       </div>
     );
   }
---
+++ src/Components/output.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class Output extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			value: props.value
+		}
+	}
+
+
+
+render(){
+	return (
+		<div className="Output">
+		 {this.props.value}
+		</div>
+		)
+       }
+     }
+
+
+ export default Output;
